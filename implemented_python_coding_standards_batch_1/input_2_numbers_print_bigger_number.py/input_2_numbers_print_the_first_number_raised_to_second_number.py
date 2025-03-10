9749e06e6a35a31f886c86f6b091aa984d23e630
Jasper <japeemga@gmail.com>
2025-03-11 02:30:31 +0800

    Revised the pseudocode, Get the result when first number is raised to the second number

--- implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_2_numbers_print_the_first_number_raised_to_second_number.py
+++ implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_2_numbers_print_the_first_number_raised_to_second_number.py
@@ -2,5 +2,7 @@
 first_number = int(input("Enter first number: "))
 second_number = int(input("Enter second number: "))
 
-#raise the first number to the second number
+#get result of first number raised to the second number using the ** operator
+result = first_number ** second_number
+
 #print the result
