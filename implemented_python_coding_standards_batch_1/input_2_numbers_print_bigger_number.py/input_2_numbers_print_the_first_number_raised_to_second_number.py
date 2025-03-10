729d44442129c000ca9c2be4d3526103aa30e50d
Jasper <japeemga@gmail.com>
2025-03-11 02:28:21 +0800

    Input 2 numbers

--- implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_2_numbers_print_the_first_number_raised_to_second_number.py
+++ implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_2_numbers_print_the_first_number_raised_to_second_number.py
@@ -1,3 +1,6 @@
 #input 2 numbers
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
 #raise the first number to the second number
 #print the result
