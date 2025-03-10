30b92d6f31057c14c71f5f1cd6f6716981e77e85
Jasper <japeemga@gmail.com>
2025-03-10 21:28:27 +0800

    Print first number if its bigger

--- implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_2_numbers_print_bigger_number.py
+++ implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_2_numbers_print_bigger_number.py
@@ -1,6 +1,9 @@
 #input 2 numbers
 first_number = int(input("Enter first number: "))
 second_number = int(input("Enter second number: "))
+
 #check which number is bigger
+if first_number > second_number:
+    print(first_number)
     #print first number if it is bigger
     #print second number if it is bigger
