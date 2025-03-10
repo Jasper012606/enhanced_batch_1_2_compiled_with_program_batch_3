ef431d825605d0ff877e410204747e8bb4217cc3
Jasper <japeemga@gmail.com>
2025-03-11 02:06:03 +0800

    Get the sum of the two numbers, changed the second line of the pseudocode

--- implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_2_numbers_print_sum_of_numbers.py
+++ implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_2_numbers_print_sum_of_numbers.py
@@ -1,5 +1,8 @@
 #input 2 numbers
 first_number = int(input("Enter first number: "))
 second_number = int(input("Enter second number: "))
-#add first number to the second number
+
+#get the sum of the first and second number
+sum = first_number + second_number
+
 #print sum
