46694a8733e760a04667972768425834c195e3d1
Jasper <japeemga@gmail.com>
2025-03-11 02:23:26 +0800

    Get the quotient of the numbers

--- implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_2_numbers_print_quotient_with_decimal_point.py
+++ implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_2_numbers_print_quotient_with_decimal_point.py
@@ -3,4 +3,6 @@ dividend = int(input("Enter first number: "))
 divisor = int(input("Enter second number: "))   
 
 #get the quotient of the 2 numbers with decimal point
+quotient = dividend / divisor
+
 #print quotient
