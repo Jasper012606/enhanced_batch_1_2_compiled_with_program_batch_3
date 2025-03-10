879ba08b9f8663f47dc129153537d6e3b19a21a8
Jasper <japeemga@gmail.com>
2025-03-11 02:41:09 +0800

    Initialize sum to 0 first

--- implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_10_numbers_print_sum_of_all_numbers.py
+++ implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_10_numbers_print_sum_of_all_numbers.py
@@ -1,8 +1,8 @@
+#initialize sum to 0
+sum = 0
 #input 10 numbers
 for i in range(10):
     number_to_add = int(input("Enter number: "))
-#initialize sum to 0
-    sum = 0
     #add each number to sum
     sum += number_to_add
 #print sum
