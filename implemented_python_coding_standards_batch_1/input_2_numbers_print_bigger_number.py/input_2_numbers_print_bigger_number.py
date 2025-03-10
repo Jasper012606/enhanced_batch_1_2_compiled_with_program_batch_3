8c559498d837dd622ae2da1e0f32befe56f5a99e
Jasper <japeemga@gmail.com>
2025-03-10 21:24:27 +0800

    Input 2 numbers

---
+++ implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_2_numbers_print_bigger_number.py
@@ -0,0 +1,6 @@
+#input 2 numbers
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+#check which number is bigger
+    #print first number if it is bigger
+    #print second number if it is bigger
