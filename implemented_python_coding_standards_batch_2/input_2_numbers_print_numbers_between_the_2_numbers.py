d15c6cdbc8fc46e834f6a6f3385b307ce4df0174
Jasper <japeemga@gmail.com>
2025-03-12 00:04:00 +0800

    Input two numbers

--- implemented_python_coding_standards_batch_2/input_2_numbers_print_numbers_between_the_2_numbers.py
+++ implemented_python_coding_standards_batch_2/input_2_numbers_print_numbers_between_the_2_numbers.py
@@ -1,4 +1,6 @@
 #input two  numbers
+first_number = int(input("Enter a number: "))
+second_number = int(input("Enter another number: "))
 #check which number is greater
     #if first number is greater
         #print all numbers between the second number and first number
