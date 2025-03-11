7fb0ae7a38b65f193548209bb47589c4b4976da6
Jasper <japeemga@gmail.com>
2025-03-12 05:15:07 +0800

    Input 10 numbers

--- compilation_of_programs_batch_3/input_10_numbers_display_number_that_has_no_duplicate.py
+++ compilation_of_programs_batch_3/input_10_numbers_display_number_that_has_no_duplicate.py
@@ -1,6 +1,8 @@
 #make an empty list
 numbers = []
 #input 10 numbers
+for i in range(10):
+    number = int(input("Enter a number: "))
     #add numbers to the list
 #make an empty list of numbers without duplicates
 #check if number in numbers has no duplicates
