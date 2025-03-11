d5cd5d1e5484afde2f87200891b2c65053e2c520
Jasper <japeemga@gmail.com>
2025-03-11 17:55:59 +0800

    Set result as first number minus remaining numbers

--- implemented_python_coding_standards_batch_2/input_10_numbers_print_result_of_first_number_subrtracted_by_remaining_numbers.py
+++ implemented_python_coding_standards_batch_2/input_10_numbers_print_result_of_first_number_subrtracted_by_remaining_numbers.py
@@ -3,5 +3,6 @@ first_number = int(input("Enter a number: "))
 #input 9 remaining numbers
 for remainding_numbers in range(9):
     remainding_numbers = int(input("Enter a number: "))
-#subtract each number to the first number
+    #set the result as the difference of the first number subtracted by the remaining numbers
+    result = first_number - remainding_numbers
 #print result
