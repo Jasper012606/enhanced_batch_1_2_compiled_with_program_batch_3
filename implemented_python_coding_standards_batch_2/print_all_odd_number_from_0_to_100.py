090e2dc25525bee67dc884fb13722222f13a0701
Jasper <japeemga@gmail.com>
2025-03-11 23:36:06 +0800

    Check if odd number, print if true

--- implemented_python_coding_standards_batch_2/print_all_odd_number_from_0_to_100.py
+++ implemented_python_coding_standards_batch_2/print_all_odd_number_from_0_to_100.py
@@ -1,8 +1,9 @@
 #initialize number to 0
 number = 0
 #print all numbers from 0 to 100
-while number < 101:
-    print(number)
-    number += 1
-#check if odd
-    #print if true
+while number < 100:
+    #check if odd
+    if number % 2 != 0:
+        #print if true
+        print(number)
+        number += 1
