8a211df1aee710af9cdc7f7740e7c6b17cb03f77
Jasper <japeemga@gmail.com>
2025-03-11 15:51:38 +0800

    Get the quotient without decimal

--- implemented_python_coding_standards_batch_2/input_2_numbers_print_quotient_without_decimal.py
+++ implemented_python_coding_standards_batch_2/input_2_numbers_print_quotient_without_decimal.py
@@ -1,6 +1,6 @@
 #Input 2 numbers
 dividend = int(input("Enter a number:"))
 divisor = int(input("Enter another number: "))
-
 #get the quotient without the decimal
+quotient = dividend // divisor
 #print the quotient
