cb90644d1609fcf650085c91cff9f054a37fdc42
Jasper <japeemga@gmail.com>
2025-03-12 00:02:49 +0800

    Pseudocode of program 10

---
+++ implemented_python_coding_standards_batch_2/input_2_numbers_print_numbers_between_the_2_numbers.py
@@ -0,0 +1,6 @@
+#input two  numbers
+#check which number is greater
+    #if first number is greater
+        #print all numbers between the second number and first number
+    #if second number is greater
+        #print all numbers between the first number and second number
