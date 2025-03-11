31bc5957815f6e06e81ee9daccc4304aa1148471
Jasper <japeemga@gmail.com>
2025-03-11 17:53:41 +0800

    Input 9 remainding numbers

--- implemented_python_coding_standards_batch_2/input_10_numbers_print_result_of_first_number_subrtracted_by_remaining_numbers.py
+++ implemented_python_coding_standards_batch_2/input_10_numbers_print_result_of_first_number_subrtracted_by_remaining_numbers.py
@@ -1,5 +1,7 @@
 #initialize the first number
 first_number = int(input("Enter a number: "))
 #input 9 remaining numbers
+for remainding_numbers in range(9):
+    remainding_numbers = int(input("Enter a number: "))
 #subtract each number to the first number
 #print result
