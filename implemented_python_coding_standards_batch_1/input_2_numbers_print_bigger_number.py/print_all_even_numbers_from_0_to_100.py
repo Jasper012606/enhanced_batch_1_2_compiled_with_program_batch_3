0aae5d80c23c2e6d46988e2b947e3c0e71350f6b
Jasper <japeemga@gmail.com>
2025-03-11 12:37:43 +0800

    Changed variable to 'number'

--- implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/print_all_even_numbers_from_0_to_100.py
+++ implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/print_all_even_numbers_from_0_to_100.py
@@ -1,6 +1,6 @@
 #print all numbers from 0 to 100
-for i in range(101):
+for number in range(101):
     #check if number is even
-    if i % 2 == 0:
+    if number % 2 == 0:
         #print if true
-        print(i)
+        print(number)
