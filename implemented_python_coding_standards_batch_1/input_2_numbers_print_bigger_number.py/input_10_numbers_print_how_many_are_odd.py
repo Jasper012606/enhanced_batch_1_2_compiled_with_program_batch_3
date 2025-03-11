6de03ce0829e91c7979712f49f07f4634ce117af
Jasper <japeemga@gmail.com>
2025-03-11 12:19:02 +0800

    Input 10 numbers

--- implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_10_numbers_print_how_many_are_odd.py
+++ implemented_python_coding_standards_batch_1/input_2_numbers_print_bigger_number.py/input_10_numbers_print_how_many_are_odd.py
@@ -1,6 +1,8 @@
 #initialize odd_count to 0
 odd_count = 0
 #input 10 numbers
+for i in range(10):
+    number = int(input("Enter number: "))
 #check if odd
     #add to odd_count if true
 #print odd_count
