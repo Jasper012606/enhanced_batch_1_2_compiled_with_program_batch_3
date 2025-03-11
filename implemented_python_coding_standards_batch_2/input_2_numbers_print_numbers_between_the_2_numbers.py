cb3f064f4091b9eb163c6f988e2952d353d70dea
Jasper <japeemga@gmail.com>
2025-03-12 00:08:32 +0800

    Print numbers between the 2 numbers

--- implemented_python_coding_standards_batch_2/input_2_numbers_print_numbers_between_the_2_numbers.py
+++ implemented_python_coding_standards_batch_2/input_2_numbers_print_numbers_between_the_2_numbers.py
@@ -5,6 +5,10 @@ second_number = int(input("Enter another number: "))
     #if first number is greater
 if first_number > second_number:
         #print all numbers between the second number and first number
+    for i in range(second_number, first_number):
+        print(i)
 elif first_number < second_number:
+    for i in range(first_number, second_number):
+        print(i)
     #if second number is greater
         #print all numbers between the first number and second number
