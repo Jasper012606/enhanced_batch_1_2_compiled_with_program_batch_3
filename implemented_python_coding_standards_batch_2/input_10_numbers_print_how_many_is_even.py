222cb2b34fa07f70b647402c46e282b39d2c426d
Jasper <japeemga@gmail.com>
2025-03-11 18:17:36 +0800

    Input 10 numbers

--- implemented_python_coding_standards_batch_2/input_10_numbers_print_how_many_is_even.py
+++ implemented_python_coding_standards_batch_2/input_10_numbers_print_how_many_is_even.py
@@ -1,6 +1,8 @@
 #initialize even count to 0
 even_count = 0
 #input 10 numbers
+for i in range(10):
+    number = int(input("Enter a number: "))
     #check if even
         #add 1 to even count if true
 #print even count
