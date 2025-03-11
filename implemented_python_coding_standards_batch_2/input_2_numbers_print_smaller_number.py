b178b74fb030cc055f086caa68bb50343df400ea
Jasper <japeemga@gmail.com>
2025-03-11 12:55:52 +0800

    Print smaller number

--- implemented_python_coding_standards_batch_2/input_2_numbers_print_smaller_number.py
+++ implemented_python_coding_standards_batch_2/input_2_numbers_print_smaller_number.py
@@ -4,4 +4,7 @@ second_number = int(input("Enter second number: "))
 #check which number is smaller
 if first_number < second_number:
     #print first number if it is smaller
+    print(first_number)
+else:
     #print second number if it is smaller
+    print(second_number)
