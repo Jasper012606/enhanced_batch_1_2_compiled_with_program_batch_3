ed1a59f4c86cba1a4c44d282a7aff41810bca1e8
Jasper <japeemga@gmail.com>
2025-03-11 15:42:48 +0800

    Check if numbers are not the same

--- implemented_python_coding_standards_batch_2/input_2_numbers_print_not_equal_if_nubmers_are_not_same.py
+++ implemented_python_coding_standards_batch_2/input_2_numbers_print_not_equal_if_nubmers_are_not_same.py
@@ -2,4 +2,5 @@
 first_number = int(input("Enter first number: "))
 second_number = int(input("Enter second number: "))
 #check if numbers are not same
+if first_number != second_number:
     #print not equal if true
