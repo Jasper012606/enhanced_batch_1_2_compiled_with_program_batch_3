0bdd151880f12d903df39f9b49d6d9789be4e546
Jasper <japeemga@gmail.com>
2025-03-11 12:52:48 +0800

    Input 2 nubmers

--- implemented_python_coding_standards_batch_2/input_2_numbers_print_smaller_number.py
+++ implemented_python_coding_standards_batch_2/input_2_numbers_print_smaller_number.py
@@ -1,4 +1,6 @@
 #input 2 numbers
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
 #check which number is smaller
     #print first number if it is smaller
     #print second number if it is smaller
