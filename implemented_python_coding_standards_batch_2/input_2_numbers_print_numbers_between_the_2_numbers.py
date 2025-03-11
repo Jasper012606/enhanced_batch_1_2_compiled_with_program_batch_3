6994dc7370e0e116cbf2754de0fe499c7d3afa5e
Jasper <japeemga@gmail.com>
2025-03-12 00:05:34 +0800

    Check which number is greater

--- implemented_python_coding_standards_batch_2/input_2_numbers_print_numbers_between_the_2_numbers.py
+++ implemented_python_coding_standards_batch_2/input_2_numbers_print_numbers_between_the_2_numbers.py
@@ -3,6 +3,8 @@ first_number = int(input("Enter a number: "))
 second_number = int(input("Enter another number: "))
 #check which number is greater
     #if first number is greater
+if first_number > second_number:
         #print all numbers between the second number and first number
+elif first_number < second_number:
     #if second number is greater
         #print all numbers between the first number and second number
