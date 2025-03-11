43ff1963a49d3e55482c56e4f00b6e708396b898
Jasper <japeemga@gmail.com>
2025-03-12 05:13:43 +0800

    Pseudocode of program 1

---
+++ compilation_of_programs_batch_3/input_10_numbers_display_number_that_has_no_duplicate.py
@@ -0,0 +1,7 @@
+#make an empty list
+#input 10 numbers
+    #add numbers to the list
+#make an empty list of numbers without duplicates
+#check if number in numbers has no duplicates
+    #add number to the list if true
+#print numbers without duplicates
