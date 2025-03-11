cc4ecfc31846e71dce782ce295708a0e65f3d583
Jasper <japeemga@gmail.com>
2025-03-11 18:19:11 +0800

    Check if even number, add one to even count if true

--- implemented_python_coding_standards_batch_2/input_10_numbers_print_how_many_is_even.py
+++ implemented_python_coding_standards_batch_2/input_10_numbers_print_how_many_is_even.py
@@ -4,5 +4,7 @@ even_count = 0
 for i in range(10):
     number = int(input("Enter a number: "))
     #check if even
+    if number % 2 == 0: 
         #add 1 to even count if true
+        even_count += 1
 #print even count
