f3fdbf5781042f033950f0cddba597a70e7c554d
Jasper <japeemga@gmail.com>
2025-03-12 05:20:29 +0800

    Make an empty list of numbers without duplicates

--- compilation_of_programs_batch_3/input_10_numbers_display_number_that_has_no_duplicate.py
+++ compilation_of_programs_batch_3/input_10_numbers_display_number_that_has_no_duplicate.py
@@ -5,7 +5,9 @@ for i in range(10):
     number = int(input("Enter a number: "))
     #add numbers to the list
     list_of_numbers.append(number)
+    
 #make an empty list of numbers without duplicates
+number_without_duplicates = []
 #check if number in numbers has no duplicates
     #add number to the list if true
 #print numbers without duplicates
