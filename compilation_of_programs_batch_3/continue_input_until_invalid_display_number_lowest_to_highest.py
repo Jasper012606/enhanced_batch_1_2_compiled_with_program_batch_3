8d22b2432c54e8a7b670b2912649b3184d8fb804
Jasper <japeemga@gmail.com>
2025-03-13 07:55:15 +0800

    Pseudocode of program 5

--- compilation_of_programs_batch_3/continue_input_until_invalid_display_number_lowest_to_highest.py
+++ compilation_of_programs_batch_3/continue_input_until_invalid_display_number_lowest_to_highest.py
@@ -1,7 +1,7 @@
 #make an empty list of numbers
 #make a function that displays the number lowest to highest
     #make a loop that asks for input until the input is invalid and print numbers lowest to highest
-        #check if input is not in the list of number
+            #check if input is not in the list of number
             #add input to list of numbers if true
     #if input is invalid, sort the list of numbers
     #print the list of numbers
