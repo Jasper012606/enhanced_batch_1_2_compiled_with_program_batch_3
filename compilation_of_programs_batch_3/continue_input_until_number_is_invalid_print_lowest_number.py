de5e76ccbe84710931b480c2ddff813f6b8a4dfe
Jasper <japeemga@gmail.com>
2025-03-13 06:41:55 +0800

    Update pseudocode

--- compilation_of_programs_batch_3/continue_input_until_number_is_invalid_print_lowest_number.py
+++ compilation_of_programs_batch_3/continue_input_until_number_is_invalid_print_lowest_number.py
@@ -1,5 +1,5 @@
-#make an empty list of numbers
-#make a function that asks user to input number until the number is invalid
+#make a function that gets the lowest number from the input
+    #make a loop that asks for input until the input is invalid
     #initialize lowest number to None
     #if lowest number is None, set input to lowest number
         #check if next input is less than lowest number
