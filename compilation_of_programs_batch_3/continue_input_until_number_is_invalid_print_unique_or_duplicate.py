ec1177759a0e07ae82d15ba2e12f99be0013b70f
Jasper <japeemga@gmail.com>
2025-03-13 05:17:33 +0800

    Made empty list of numbers, ask user for number input until invalid

--- compilation_of_programs_batch_3/continue_input_until_number_is_invalid_print_unique_or_duplicate.py
+++ compilation_of_programs_batch_3/continue_input_until_number_is_invalid_print_unique_or_duplicate.py
@@ -1,9 +1,16 @@
 #make an empty list
+list_of_numbers = []
 #define a function that asks user to input a number until the number is invalid
+def valid_number():
+    while True:
+        try:
+            number = int(input("Enter a number: "))
+        except ValueError:
+            break
         #check if input is valid
             #check if number is in the list
                 #print duplicate if true
                 #print unique if not true
                     #add number to the list
 #call the function
- 
+valid_number()
