edcda5ea97a9c5f854a955701d4a4d213c8eadf5
Jasper <japeemga@gmail.com>
2025-03-12 18:49:00 +0800

    Check if the input is not in the list, add to the list if true

--- compilation_of_programs_batch_3/input_10_numbers_display_all_numbers_but_only_display_first_entry_for_numbers_with_duplicate.py
+++ compilation_of_programs_batch_3/input_10_numbers_display_all_numbers_but_only_display_first_entry_for_numbers_with_duplicate.py
@@ -3,6 +3,8 @@ list_of_numbers = []
 #input 10 numbers
 for i in range(10):
     number = int(input("Enter a number: "))
-    #check if number is in the list
-        #add number to the list if no true
+    #check if number is not in the list
+    if number not in list_of_numbers:
+        list_of_numbers.append(number)
+        #add number to the list if true
 #print the numbers in the list
