458a029f9a57ae127f96e56e7b99021c5399b8f1
Jasper <japeemga@gmail.com>
2025-03-13 06:54:17 +0800

    Function that gets lowest number, loop that ask user until input is invalid

--- compilation_of_programs_batch_3/continue_input_until_number_is_invalid_print_lowest_number.py
+++ compilation_of_programs_batch_3/continue_input_until_number_is_invalid_print_lowest_number.py
@@ -1,7 +1,15 @@
 #make a function that gets the lowest number from the input
-    #make a loop that asks for input until the input is invalid
+def get_lowest_number():
     #initialize lowest number to None
-    #if lowest number is None, set input to lowest number
-        #check if next input is less than lowest number
+    lowest_number = None
+    #make a loop that asks for input until the input is invalid and print the lowest number
+    while True:
+        try:
+            number = int(input("Enter a number: "))
+        #if lowest number is None, set input to lowest number
+            #check if next input is less than lowest number
             #set input to lowest number if true
+        except ValueError:
+            break
 #call the function
+get_lowest_number()
