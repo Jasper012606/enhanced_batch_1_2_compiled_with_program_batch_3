9ab7d438bf1aa2307f2328691d956f17d7fb12b9
Jasper <japeemga@gmail.com>
2025-03-13 05:35:32 +0800

    Check if number is in the list, print duplicate if true, print unique if not true then add number to list

--- compilation_of_programs_batch_3/continue_input_until_number_is_invalid_print_unique_or_duplicate.py
+++ compilation_of_programs_batch_3/continue_input_until_number_is_invalid_print_unique_or_duplicate.py
@@ -4,12 +4,17 @@ list_of_numbers = []
 def valid_number():
     while True:
         try:
-            number = int(input("Enter a number: "))
         #check if input is valid
+            number = int(input("Enter a number: "))
             #check if number is in the list
+            if number in list_of_numbers:
                 #print duplicate if true
+                print("Duplicate")
                 #print unique if not true
+            else:
+                print("Unique")
                     #add number to the list
+                list_of_numbers.append(number)
         except ValueError:
             break
 #call the function
