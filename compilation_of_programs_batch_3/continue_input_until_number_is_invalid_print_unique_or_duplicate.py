de3fa1addb31bcdbc127b29e710d5f1f9c7b0b09
Jasper <japeemga@gmail.com>
2025-03-13 05:13:04 +0800

    Pseudocode of program 3

---
+++ compilation_of_programs_batch_3/continue_input_until_number_is_invalid_print_unique_or_duplicate.py
@@ -0,0 +1,9 @@
+#make an empty list
+#define a function that asks user to input a number until the number is invalid
+        #check if input is valid
+            #check if number is in the list
+                #print duplicate if true
+                #print unique if not true
+                    #add number to the list
+#call the function
+ 
