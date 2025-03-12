219e91a5284cad66979c1caf8d9d31aaa2fc3ad1
Jasper <japeemga@gmail.com>
2025-03-12 18:49:46 +0800

    Print the list of numbers

--- compilation_of_programs_batch_3/input_10_numbers_display_all_numbers_but_only_display_first_entry_for_numbers_with_duplicate.py
+++ compilation_of_programs_batch_3/input_10_numbers_display_all_numbers_but_only_display_first_entry_for_numbers_with_duplicate.py
@@ -5,6 +5,7 @@ for i in range(10):
     number = int(input("Enter a number: "))
     #check if number is not in the list
     if number not in list_of_numbers:
-        list_of_numbers.append(number)
         #add number to the list if true
+        list_of_numbers.append(number)
 #print the numbers in the list
+print(list_of_numbers)
