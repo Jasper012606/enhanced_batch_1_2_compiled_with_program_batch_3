df9003c738584298b8145cd6ebd64673dcc673cd
Jasper <japeemga@gmail.com>
2025-03-12 18:43:48 +0800

    Make an empty list, input 10 numbers

--- compilation_of_programs_batch_3/input_10_numbers_display_all_numbers_but_only_display_first_entry_for_numbers_with_duplicate.py
+++ compilation_of_programs_batch_3/input_10_numbers_display_all_numbers_but_only_display_first_entry_for_numbers_with_duplicate.py
@@ -1,5 +1,8 @@
 #make an empty list 
+list_of_numbers = []
 #input 10 numbers
+for i in range(10):
+    number = int(input("Enter a number: "))
     #check if number is in the list
         #add number to the list if no true
 #print the numbers in the list
