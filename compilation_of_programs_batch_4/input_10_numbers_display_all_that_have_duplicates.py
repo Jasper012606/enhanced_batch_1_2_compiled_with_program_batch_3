626ec85f0ef7a1a5eef449dd62765af71b57a824
Jasper <japeemga@gmail.com>
2025-03-13 12:05:58 +0800

    Make empty list for numbers and numbers with duplicates

--- compilation_of_programs_batch_4/input_10_numbers_display_all_that_have_duplicates.py
+++ compilation_of_programs_batch_4/input_10_numbers_display_all_that_have_duplicates.py
@@ -1,5 +1,7 @@
 #make an empty list of numbers
+list_of_numbers = []
 #make an empty list of numbers with duplicates
+numbers_with_duplicates = []
 #input 10 numbers
     #check if number is not in the list
         #add number to the list if true
