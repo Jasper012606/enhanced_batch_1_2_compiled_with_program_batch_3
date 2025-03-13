d1ee1ce0f03a587d474aad265032017e523c90a1
Jasper <japeemga@gmail.com>
2025-03-13 17:58:47 +0800

    Initialize highest number to None

--- compilation_of_programs_batch_4/continue_input_until_invalid_display_highest_number.py
+++ compilation_of_programs_batch_4/continue_input_until_invalid_display_highest_number.py
@@ -1,6 +1,9 @@
 #define a function that gets the highest number
-#initialize highest number to None
-#ask for input until input is invalid then print the highest number
-    #set first input to highest number if highest number is None
-    #check if next input is greater than highest number
-        #set input to highest number if true
+def get_highest_number():
+    #initialize highest number to None
+    highest_number = None
+    #ask for input until input is invalid then print the highest number
+        #set first input to highest number if highest number is None
+        #check if next input is greater than highest number
+            #set input to highest number if true
+get_highest_number()
