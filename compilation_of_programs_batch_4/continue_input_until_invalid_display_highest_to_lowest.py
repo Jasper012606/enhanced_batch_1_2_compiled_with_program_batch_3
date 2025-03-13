6c3789cc0035563004b52f82919cccee86c3b65b
Jasper <japeemga@gmail.com>
2025-03-13 18:09:57 +0800

    Make an empty list of numbers, ask user for input until input is invalid

--- compilation_of_programs_batch_4/continue_input_until_invalid_display_highest_to_lowest.py
+++ compilation_of_programs_batch_4/continue_input_until_invalid_display_highest_to_lowest.py
@@ -1,7 +1,15 @@
 #define a function that gets the highest to lowest number from the input
-#make an empty list of numbers
-#ask user for input until input is invalid
-#add input to the list
-#if input is invalid
-    #sort the list of numbers
-    #print the list of numbers
+def highest_to_lowest():
+    #make an empty list of numbers
+    list_of_numbers = []
+    #ask user for input until input is invalid
+    while True:
+        try:
+            number = int(input("Enter a number: "))
+    #add input to the list
+        except ValueError:
+            break
+    #if input is invalid
+        #sort the list of numbers
+        #print the list of numbers
+highest_to_lowest()
