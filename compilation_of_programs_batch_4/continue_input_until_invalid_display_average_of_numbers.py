f825c3b03c41edfae4d48e82bb60d9f08ac8212f
Jasper <japeemga@gmail.com>
2025-03-13 18:23:15 +0800

    Pseudocode of program 5

---
+++ compilation_of_programs_batch_4/continue_input_until_invalid_display_average_of_numbers.py
@@ -0,0 +1,7 @@
+#define a function that gets the average of numbers from the input
+#initialize number count to 0
+#initialize sum of numbers to 0
+#ask user for input until input is invalid
+#for every input, add number to the first number, add 1 to average
+#divide the sum of numbers by the average
+#print the average
