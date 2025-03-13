c8783b23451aae176ddd48307f99bbba0fb5dd14
Jasper <japeemga@gmail.com>
2025-03-13 12:04:20 +0800

    Pseudocode of program 1

---
+++ compilation_of_programs_batch_4/input_10_numbers_display_all_that_have_duplicates.py
@@ -0,0 +1,8 @@
+#make an empty list of numbers
+#make an empty list of numbers with duplicates
+#input 10 numbers
+    #check if number is not in the list
+        #add number to the list if true
+        #add number to the list of numbers with duplicates if not true
+#print the numbers with duplicates
+
