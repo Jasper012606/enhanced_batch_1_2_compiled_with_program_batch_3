a389a0bef8aefba69fd3845296b58db242684cbb
Jasper <japeemga@gmail.com>
2025-03-13 17:59:57 +0800

    Ask user for input until input is invalid

--- compilation_of_programs_batch_4/continue_input_until_invalid_display_highest_number.py
+++ compilation_of_programs_batch_4/continue_input_until_invalid_display_highest_number.py
@@ -3,7 +3,12 @@ def get_highest_number():
     #initialize highest number to None
     highest_number = None
     #ask for input until input is invalid then print the highest number
+    while True:
+        try:
+            number = int(input("Enter a number: "))
         #set first input to highest number if highest number is None
         #check if next input is greater than highest number
             #set input to highest number if true
+        except ValueError:
+            break
 get_highest_number()
