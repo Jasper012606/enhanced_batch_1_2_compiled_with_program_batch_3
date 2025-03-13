a06335bca6ea4e2d9c57ea048eb8c5b07f59f4bf
Jasper <japeemga@gmail.com>
2025-03-13 17:42:12 +0800

    Pseudocode of program 2

---
+++ compilation_of_programs_batch_4/continue_input_until_invalid_display_number_with_most_duplicates.py
@@ -0,0 +1,6 @@
+#define a function that gets the number with the most duplicates from the input
+#make an empy list of numbers
+#ask user for input until input is invalid
+#add input to the list
+#check which number in the list has the most duplicates
+#print the number with the most duplicates
