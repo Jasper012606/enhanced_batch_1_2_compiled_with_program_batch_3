6f1bcc35d1c25c9bf0889bdbff112eb0293ab3fd
Jasper <japeemga@gmail.com>
2025-03-13 18:24:32 +0800

    Initialize number count and sum of numbers to 0

--- compilation_of_programs_batch_4/continue_input_until_invalid_display_average_of_numbers.py
+++ compilation_of_programs_batch_4/continue_input_until_invalid_display_average_of_numbers.py
@@ -1,7 +1,11 @@
 #define a function that gets the average of numbers from the input
-#initialize number count to 0
-#initialize sum of numbers to 0
-#ask user for input until input is invalid
-#for every input, add number to the first number, add 1 to average
-#divide the sum of numbers by the average
-#print the average
+def average_of_numbers():
+    #initialize number count to 0
+    number_count = 0
+    #initialize sum of numbers to 0
+    sum_of_numbers = 0
+    #ask user for input until input is invalid
+    #for every input, add number to the first number, add 1 to average
+    #divide the sum of numbers by the average
+    #print the average
+average_of_numbers()
