11f8ffe5e1a763e29437acdf736be7c51eeae5e2
Jasper <japeemga@gmail.com>
2025-03-13 12:07:31 +0800

    Input 10 numbers

--- compilation_of_programs_batch_4/input_10_numbers_display_all_that_have_duplicates.py
+++ compilation_of_programs_batch_4/input_10_numbers_display_all_that_have_duplicates.py
@@ -3,8 +3,9 @@ list_of_numbers = []
 #make an empty list of numbers with duplicates
 numbers_with_duplicates = []
 #input 10 numbers
+for i in range(10):
+    number = int(input("Enter a number: "))
     #check if number is not in the list
         #add number to the list if true
         #add number to the list of numbers with duplicates if not true
 #print the numbers with duplicates
-
