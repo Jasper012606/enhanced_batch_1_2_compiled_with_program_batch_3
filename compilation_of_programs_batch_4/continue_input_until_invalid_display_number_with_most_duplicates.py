7a391cf2ec9c5aeba0cda3b327272b675da363e7
Jasper <japeemga@gmail.com>
2025-03-13 17:52:20 +0800

    Check if list is empty, print 'No number entered' if true

--- compilation_of_programs_batch_4/continue_input_until_invalid_display_number_with_most_duplicates.py
+++ compilation_of_programs_batch_4/continue_input_until_invalid_display_number_with_most_duplicates.py
@@ -9,10 +9,16 @@ def number_with_most_duplicates():
             #add input to the list
             list_of_numbers.append(number)
         except ValueError:
-            #check which number in the list has the most duplicates
-            number_with_most_duplicates = max(list_of_numbers, key = list_of_numbers.count)
-            #print the number with the most duplicates
-            print(number_with_most_duplicates)
-            break
+            #check if the list is not empty
+            if list_of_numbers:
+                #check which number in the list has the most duplicates
+                number_with_most_duplicates = max(list_of_numbers, key = list_of_numbers.count)
+                #print the number with the most duplicates
+                print(number_with_most_duplicates)
+                break
+            #if the list is empty, print no number entered
+            else:
+                print("No number entered")
+                
 
 number_with_most_duplicates()
