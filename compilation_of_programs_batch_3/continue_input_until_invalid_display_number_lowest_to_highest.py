a688f114d648b461331673c7ba752bc0ea49e7a2
Jasper <japeemga@gmail.com>
2025-03-13 08:06:31 +0800

    Make empty list of numbers, define a function that prints lowest to highest of the numbers

--- compilation_of_programs_batch_3/continue_input_until_invalid_display_number_lowest_to_highest.py
+++ compilation_of_programs_batch_3/continue_input_until_invalid_display_number_lowest_to_highest.py
@@ -3,13 +3,8 @@ list_of_numbers = []
 #make a function that displays the number lowest to highest
 def lowest_to_highest():
     #make a loop that asks for input until the input is invalid and print numbers lowest to highest
-    while True:
-        try:
-            number = int(input("Enter a number: "))
             #check if input is not in the list of number
             #add input to list of numbers if true
-        except ValueError:
-            break
     #if input is invalid, sort the list of numbers
     #print the list of numbers
 #call the function
