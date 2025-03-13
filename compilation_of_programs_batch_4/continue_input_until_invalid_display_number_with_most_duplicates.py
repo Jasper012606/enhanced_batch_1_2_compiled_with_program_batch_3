ae47196629c2047b86b3f53ff2858a085a25d0e6
Jasper <japeemga@gmail.com>
2025-03-13 17:45:40 +0800

    Make empty list of numbers, ask user for input until invalid

--- compilation_of_programs_batch_4/continue_input_until_invalid_display_number_with_most_duplicates.py
+++ compilation_of_programs_batch_4/continue_input_until_invalid_display_number_with_most_duplicates.py
@@ -1,6 +1,14 @@
 #define a function that gets the number with the most duplicates from the input
-#make an empy list of numbers
+def number_with_most_duplicates():
+    #make an empy list of numbers
+    list_of_numbers = []
 #ask user for input until input is invalid
+    while True:
+        try:
+            number = int(input("Enter a number: "))
+        except ValueError:
+            break
 #add input to the list
 #check which number in the list has the most duplicates
 #print the number with the most duplicates
+number_with_most_duplicates()
