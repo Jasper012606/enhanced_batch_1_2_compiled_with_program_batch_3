2bec6979bb54c3768f67cfbde21325d1aa5b19c2
Jasper <japeemga@gmail.com>
2025-03-13 18:07:15 +0800

    Pseudocode of program 4

---
+++ compilation_of_programs_batch_4/continue_input_until_invalid_display_highest_to_lowest.py
@@ -0,0 +1,7 @@
+#define a function that gets the highest to lowest number from the input
+#make an empty list of numbers
+#ask user for input until input is invalid
+#add input to the list
+#if input is invalid
+    #sort the list of numbers
+    #print the list of numbers
