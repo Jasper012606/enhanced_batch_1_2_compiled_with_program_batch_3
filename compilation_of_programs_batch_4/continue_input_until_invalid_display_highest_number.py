3f1957a3abcc5ff57d5642f474da53a195effe83
Jasper Gabriel <japeemga@gmail.com>
2025-03-13 20:48:27 +0800

    Change == to is

--- compilation_of_programs_batch_4/continue_input_until_invalid_display_highest_number.py
+++ compilation_of_programs_batch_4/continue_input_until_invalid_display_highest_number.py
@@ -8,7 +8,7 @@ def get_highest_number():
             number = int(input("Enter a number: "))
             #set first input to highest number if highest number is None
             #check if next input is greater than highest number
-            if highest_number == None or number > highest_number:
+            if highest_number is None or number > highest_number:
                 #set input to highest number if true
                 highest_number = number
         except ValueError:
