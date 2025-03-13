e15b94ea9cb879ba61f014599f93fc99451f598e
Jasper <japeemga@gmail.com>
2025-03-13 18:03:08 +0800

    Set first input as highest number, check if next input is greater than highest number, set the input as the highest number if true

--- compilation_of_programs_batch_4/continue_input_until_invalid_display_highest_number.py
+++ compilation_of_programs_batch_4/continue_input_until_invalid_display_highest_number.py
@@ -6,9 +6,11 @@ def get_highest_number():
     while True:
         try:
             number = int(input("Enter a number: "))
-        #set first input to highest number if highest number is None
-        #check if next input is greater than highest number
-            #set input to highest number if true
+            #set first input to highest number if highest number is None
+            #check if next input is greater than highest number
+            if highest_number == None or number > highest_number:
+                #set input to highest number if true
+                highest_number = number
         except ValueError:
             break
 get_highest_number()
