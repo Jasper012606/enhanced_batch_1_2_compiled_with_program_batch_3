bc3a8cfd808601727ee83537eb63c18b264ab88f
Jasper <japeemga@gmail.com>
2025-03-13 17:57:34 +0800

    Pseudocode of program 3

---
+++ compilation_of_programs_batch_4/continue_input_until_invalid_display_highest_number.py
@@ -0,0 +1,6 @@
+#define a function that gets the highest number
+#initialize highest number to None
+#ask for input until input is invalid then print the highest number
+    #set first input to highest number if highest number is None
+    #check if next input is greater than highest number
+        #set input to highest number if true
